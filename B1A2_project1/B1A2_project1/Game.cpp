b531ab9b71084f8d4da733a681cfb40d7231fb50
Im Chaeeun <dev.chaeng@gmail.com>
2024-09-30 18:29:22 +0900

    InputManager 생성 후 Game.cpp에 추가

--- B1A2_project1/B1A2_project1/Game.cpp
+++ B1A2_project1/B1A2_project1/Game.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "Game.h"
+#include "InputManager.h"
 
 Game::Game()
 {
@@ -20,10 +21,14 @@ void Game::Init(HWND hwnd)
 	_bmpBack = ::CreateCompatibleBitmap(hdc, _rect.right, _rect.bottom);	// hdc와 호환되는 비트맵 생성
 	HBITMAP prev = (HBITMAP)::SelectObject(hdcBack, _bmpBack);	// DC와 BMP 연결
 	::DeleteObject(prev);
+
+	// 각 매니저들 Init
+	GET_SINGLE(InputManager)->Init(hwnd);
 }
 
 void Game::Update()
 {
+	GET_SINGLE(InputManager)->Update();
 }
 
 void Game::Render()
---
+++ B1A2_project1/B1A2_project1/InputManager.h
@@ -0,0 +1,69 @@
+#pragma once
+// InputManager : 키보드, 마우스 입력을 처리하는 매니저
+
+enum class KeyType	// 키 버튼 
+{
+	LeftMouse = VK_LBUTTON,
+	RightMouse = VK_RBUTTON,
+
+	Up = VK_UP,
+	DOWN = VK_DOWN,
+	Left = VK_LEFT,
+	Right = VK_RIGHT,
+	SpacwBar = VK_SPACE,
+
+	KEY_1 = '1',
+	KEY_2 = '2',
+
+	W = 'W',
+	A = 'A',
+	S = 'S',
+	D = 'D',
+	Q = 'Q',
+	E = 'E'
+};
+
+enum class KeyState		// 키 상태
+{
+	None,
+	Press,
+	Down,
+	Up,
+
+	End		// KeyState의 개수
+};
+
+enum    // 개수 깔끔하게 정리
+{
+	KEY_TYPE_COUNT = static_cast<int32>(UINT8_MAX) + 1,
+	KEY_STATE_COUNT = static_cast<int32>(KeyState::End)
+};
+
+class InputManager
+{
+	DECLARE_SINGLE(InputManager);
+
+public:
+	void Init(HWND hwnd);
+	void Update();
+
+	// 누르고 있을 때
+	bool GetButton(KeyType key) { return GetState(key) == KeyState::Press; }
+
+	// 맨 처음 눌렀을 때
+	bool GetButtonDown(KeyType key) { return GetState(key) == KeyState::Down; }
+
+	// 맨 처음 눌렀다가 땔 때
+	bool GetButtonUp(KeyType key) { return GetState(key) == KeyState::Up; }
+
+	POINT GetMousePos() { return _mousePos; }
+
+private:
+	KeyState GetState(KeyType key) { return _states[static_cast<uint8>(key)]; }
+
+private:
+	HWND _hwnd = 0;
+	std::vector<KeyState> _states;
+	POINT _mousePos;
+};
+
