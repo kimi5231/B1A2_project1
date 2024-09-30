4def800672177935f46c45adeec2ca17f317aeb2
Im Chaeeun <dev.chaeng@gmail.com>
2024-09-30 18:47:19 +0900

    Scene 상위 클래스, DevScene 클래스 추가

---
+++ B1A2_project1/B1A2_project1/DevScene.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "DevScene.h"
+#include "InputManager.h"
+
+DevScene::DevScene()
+{
+}
+
+DevScene::~DevScene()
+{
+}
+
+void DevScene::Init()
+{
+}
+
+void DevScene::Update()
+{
+	Super::Update();
+}
+
+void DevScene::Render(HDC hdc)
+{
+	Super::Render(hdc);
+}
