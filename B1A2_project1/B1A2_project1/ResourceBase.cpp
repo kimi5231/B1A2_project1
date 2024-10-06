132c980fc19ac93ca0231a185e61370fed0f5298
Im Chaeeun <dev.chaeng@gmail.com>
2024-10-06 22:15:16 +0900

    Resource 필터 추가 후 ResourceBase 추가 Texture, Sprite, Flipbook 생성하면 됨.

---
+++ B1A2_project1/B1A2_project1/ResourceBase.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "ResourceBase.h"
+
+ResourceBase::ResourceBase()
+{
+}
+
+ResourceBase::~ResourceBase()
+{
+}
+
+void ResourceBase::LoadFile(const std::wstring& path)
+{
+}
+
+void ResourceBase::SaveFile(const std::wstring& path)
+{
+}
