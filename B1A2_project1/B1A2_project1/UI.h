db4494514e2d92b47c016beb41982d42de8b029c
kimi5231 <djwjod@naver.com>
2024-11-01 00:05:01 +0900

    UI 클래스에 pos, size 멤버 변수 추가 후 각각 get, set 함수 추가

--- B1A2_project1/B1A2_project1/UI.h
+++ B1A2_project1/B1A2_project1/UI.h
@@ -9,7 +9,13 @@ public:
 	virtual void Tick();
 	virtual void Render(HDC hdc);
 
-protected:
-	
-};
+public:
+	void SetPos(Vec2 pos) { _pos = pos; }
+	Vec2 GetPos() { return _pos; }
+	void SetSize(Vec2Int size) { _size = size; }
+	Vec2Int GetSize() { return _size; }
 
+protected:
+	Vec2 _pos{};
+	Vec2Int _size{};
+};
