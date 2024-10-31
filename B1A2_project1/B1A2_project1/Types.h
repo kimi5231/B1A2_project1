e30c40114ceb3ab57fb5bec70478a8ea039f6627
kimi5231 <djwjod@naver.com>
2024-10-31 23:52:41 +0900

    Types에 2차원 벡터 자료형 추가
    Vec2 == 2차원 벡터(float)
    Vec2Int == 2차원 벡터(int)

--- B1A2_project1/B1A2_project1/Types.h
+++ B1A2_project1/B1A2_project1/Types.h
@@ -8,3 +8,134 @@ using uint8 = unsigned __int8;
 using uint16 = unsigned __int16;
 using uint32 = unsigned __int32;
 using uint64 = unsigned __int64;
+
+// 2Â÷¿ø º¤ÅÍ
+struct Vec2
+{
+	float x;
+	float y;
+
+	Vec2() : x(0.f), y(0.f) {}
+	Vec2(float x, float y) : x(x), y(y) {}
+	Vec2(POINT pt) : x((float)pt.x), y((float)pt.y) {}
+	
+	Vec2 operator+(const Vec2& other)
+	{
+		Vec2 ret;
+		ret.x = x + other.x;
+		ret.y = y + other.y;
+		return ret;
+	}
+
+	Vec2 operator-(const Vec2& other)
+	{
+		Vec2 ret;
+		ret.x = x - other.x;
+		ret.y = y - other.y;
+		return ret;
+	}
+
+	Vec2 operator*(float value)
+	{
+		Vec2 ret;
+		ret.x = x * value;
+		ret.y = y * value;
+		return ret;
+	}
+
+	void operator+=(const Vec2& other)
+	{
+		x += other.x;
+		y += other.y;
+	}
+
+	void operator-=(const Vec2& other)
+	{
+		x += other.x;
+		y += other.y;
+	}
+
+	void operator*=(float value)
+	{
+		x *= value;
+		y *= value;
+	}
+
+	float LengthSquared()
+	{
+		return x * x + y * y;
+	}
+
+	float Length()
+	{
+		return ::sqrt(LengthSquared());
+	}
+
+	void Normalize()
+	{
+		float length = Length();
+		if (length < 0.0000000001f)
+			return;
+
+		x /= length;
+		y /= length;
+	}
+
+	float Dot(Vec2 other)
+	{
+		return x * other.x + y * other.y;
+	}
+
+	float Cross(Vec2 other)
+	{
+		return x * other.y - y * other.x;
+	}
+};
+
+struct Vec2Int
+{
+	int32 x;
+	int32 y;
+
+	Vec2Int() : x(0), y(0) {}
+	Vec2Int(int32 x, int32 y) : x(x), y(y) {}
+	Vec2Int(POINT pt) : x((int)pt.x), y((int)pt.y) {}
+
+	Vec2Int operator+(const Vec2Int& other)
+	{
+		Vec2Int ret;
+		ret.x = x + other.x;
+		ret.y = y + other.y;
+		return ret;
+	}
+
+	Vec2Int operator-(const Vec2Int& other)
+	{
+		Vec2Int ret;
+		ret.x = x - other.x;
+		ret.y = y - other.y;
+		return ret;
+	}
+
+	void operator+=(const Vec2Int& other)
+	{
+		x += other.x;
+		y += other.y;
+	}
+
+	void operator-=(const Vec2Int& other)
+	{
+		x += other.x;
+		y += other.y;
+	}
+
+	float Dot(Vec2 other)
+	{
+		return x * other.x + y * other.y;
+	}
+
+	float Cross(Vec2 other)
+	{
+		return x * other.y - y * other.x;
+	}
+};
