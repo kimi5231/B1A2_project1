34670593563a024dcc40a1daa3520e50d37b4825
kimi5231 <djwjod@naver.com>
2024-11-03 02:51:56 +0900

    TitleScene 클래스 추가
    부모 클래스의 overrideing된 함수를 사용 시 오류 발생

---
+++ B1A2_project1/B1A2_project1/TitleScene.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "TitleScene.h"
+
+TitleScene::TitleScene()
+{
+}
+
+TitleScene::~TitleScene()
+{
+}
+
+void TitleScene::Init()
+{
+	Super::Init();
+}
+
+void TitleScene::Update()
+{
+	Super::Update();
+}
+
+void TitleScene::Render(HDC hdc)
+{
+	Super::Render(hdc);
+}
---
+++ B1A2_project1/B1A2_project1/TitleScene.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Scene.h"
+
+class TitleScene : public Scene
+{
+	using Super = Scene;
+public:
+	TitleScene();
+	virtual ~TitleScene() override;
+
+	virtual void Init() override;
+	virtual void Update() override;
+	virtual void Render(HDC hdc) override;
+};
+
