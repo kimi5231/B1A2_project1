9807054e93573666389d039fc1efadc4825c13f0
Im Chaeeun <dev.chaeng@gmail.com>
2024-11-06 23:20:48 +0900

    SpriteActor class 구현 완료

--- B1A2_project1/B1A2_project1/SpriteActor.h
+++ B1A2_project1/B1A2_project1/SpriteActor.h
@@ -1,5 +1,24 @@
 #pragma once
-class SpriteActor
+#include "Actor.h"
+
+class Sprite;
+
+class SpriteActor : public Actor
 {
+	using Super = Actor;
+
+public:
+	SpriteActor();
+	virtual ~SpriteActor() override;
+
+	virtual void BeginPlay() override;
+	virtual void Tick() override;
+	virtual void Render(HDC hdc) override;
+
+	// Sprite로 설정하려면
+	void SetSprite(Sprite* sprite) { _sprite = sprite; }
+
+protected:
+	Sprite* _sprite = nullptr;
 };
 
