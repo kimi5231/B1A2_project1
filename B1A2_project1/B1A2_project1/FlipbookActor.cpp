229e5334a0161a172db50e5f009fe63e5e8b98e7
Im Chaeeun <dev.chaeng@gmail.com>
2024-11-07 17:14:48 +0900

    FlipbookActor 클래스 생성(cpp 파일에 구현 미완료)

--- B1A2_project1/B1A2_project1/FlipbookActor.cpp
+++ B1A2_project1/B1A2_project1/FlipbookActor.cpp
@@ -1,2 +1,30 @@
 #include "pch.h"
 #include "FlipbookActor.h"
+
+FlipbookActor::FlipbookActor()
+{
+}
+
+FlipbookActor::~FlipbookActor()
+{
+}
+
+void FlipbookActor::BeginPlay()
+{
+}
+
+void FlipbookActor::Tick()
+{
+}
+
+void FlipbookActor::Render(HDC hdc)
+{
+}
+
+void FlipbookActor::SetFlipbook(Flipbook* flipbook)
+{
+}
+
+void FlipbookActor::Reset()
+{
+}
--- B1A2_project1/B1A2_project1/FlipbookActor.h
+++ B1A2_project1/B1A2_project1/FlipbookActor.h
@@ -1,5 +1,26 @@
 #pragma once
-class FlipbookActor
+#include "Actor.h"
+
+class Flipbook;
+
+class FlipbookActor : public Actor
 {
+	using Super = Actor;
+
+public:
+	FlipbookActor();
+	virtual ~FlipbookActor() override;
+
+	virtual void BeginPlay() override;
+	virtual void Tick() override;
+	virtual void Render(HDC hdc) override;
+
+	void SetFlipbook(Flipbook* flipbook);
+	void Reset();
+
+private:
+	Flipbook* _flipbook = nullptr;
+	float _sumTime = 0.f;
+	int32 _idx = 0;		// 어디까지 틀었는지 확인
 };
 
