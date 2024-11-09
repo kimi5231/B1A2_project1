b3cb89955acdcc058f74e9b50e27fdf4766391f5
Im Chaeeun <dev.chaeng@gmail.com>
2024-11-09 13:42:03 +0900

    Player 클래스 구현 완료(마지 리소스 파일로 수정 필요)

--- B1A2_project1/B1A2_project1/Player.cpp
+++ B1A2_project1/B1A2_project1/Player.cpp
@@ -1,2 +1,53 @@
 #include "pch.h"
 #include "Player.h"
+#include "ResourceManager.h"
+#include "TimeManager.h"
+#include "InputManager.h"
+
+// *리소스 마지로 수정해야 함!
+Player::Player()
+{
+	// 젤다
+	_flipbookLeft = GET_SINGLE(ResourceManager)->GetFlipbook(L"FB_MoveLeft");
+	_flipbookRight = GET_SINGLE(ResourceManager)->GetFlipbook(L"FB_MoveRight");
+
+	// 마지
+	_flipbookPlayerRunRight = GET_SINGLE(ResourceManager)->GetFlipbook(L"FB_PlayerRunRight");
+}
+
+Player::~Player()
+{
+}
+
+void Player::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 처음 상태 지정
+	SetFlipbook(_flipbookRight);
+}
+
+void Player::Tick()
+{
+	Super::Tick();
+
+	float deltaTime = GET_SINGLE(TimeManager)->GetDeltaTime();
+
+	// *속도 기획 따라 수정하기
+	if (GET_SINGLE(InputManager)->GetButton(KeyType::A))
+	{
+		_pos.x -= 200 * deltaTime;
+		SetFlipbook(_flipbookLeft);
+	}
+	else if (GET_SINGLE(InputManager)->GetButton(KeyType::D))
+	{
+		_pos.x += 200 * deltaTime;
+		SetFlipbook(_flipbookRight);
+	}
+
+}
+
+void Player::Render(HDC hdc)
+{
+	Super::Render(hdc);
+}
--- B1A2_project1/B1A2_project1/Player.h
+++ B1A2_project1/B1A2_project1/Player.h
@@ -1,5 +1,25 @@
 #pragma once
-class Player
+
+#include "FlipbookActor.h"
+
+class Player : public FlipbookActor
 {
+	using Super = FlipbookActor;
+
+public:
+	Player();
+	virtual ~Player() override;
+
+	virtual void BeginPlay() override;
+	virtual void Tick() override;
+	virtual void Render(HDC hdc) override;
+
+private:
+	// 젤다 리소스
+	Flipbook* _flipbookLeft = nullptr;
+	Flipbook* _flipbookRight = nullptr;
+
+	// 마지 리소스
+	Flipbook* _flipbookPlayerRunRight = nullptr;
 };
 
