891cbe9bd05231631a45eae1bc90ca70dd5d329c
Lim Sooyoung <djwjod@naver.com>
2025-02-04 02:04:20 +0900

    BrokenCopyMachine 클래스 추가

---
+++ B1A2_project1/B1A2_project1/BrokenCopyMachine.cpp
@@ -0,0 +1,47 @@
+#include "pch.h"
+#include "BrokenCopyMachine.h"
+
+BrokenCopyMachine::BrokenCopyMachine()
+{
+}
+
+BrokenCopyMachine::~BrokenCopyMachine()
+{
+}
+
+void BrokenCopyMachine::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void BrokenCopyMachine::Tick()
+{
+	Super::Tick();
+
+}
+
+void BrokenCopyMachine::Render(HDC hdc)
+{
+	Super::Render(hdc);
+
+}
+
+void BrokenCopyMachine::TickIdle()
+{
+}
+
+void BrokenCopyMachine::TickNormalAttack()
+{
+}
+
+void BrokenCopyMachine::TickHit()
+{
+}
+
+void BrokenCopyMachine::TickDead()
+{
+}
+
+void BrokenCopyMachine::UpdateAnimation()
+{
+}
---
+++ B1A2_project1/B1A2_project1/BrokenCopyMachine.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Monster.h"
+
+class BrokenCopyMachine : public Monster
+{
+	using Super = Monster;
+public:
+	BrokenCopyMachine();
+	virtual ~BrokenCopyMachine() override;
+
+	virtual void BeginPlay() override;
+	virtual void Tick() override;
+	virtual void Render(HDC hdc) override;
+
+private:
+	virtual void TickIdle() override;
+	virtual void TickNormalAttack() override;
+	virtual void TickHit() override;
+	virtual void TickDead() override;
+	virtual void UpdateAnimation() override;
+
+private:
+	// Flipbook
+	Flipbook* _flipbookIdle[2] = {};
+	Flipbook* _flipbookNormalAttack[2] = {};
+	Flipbook* _flipbookHit[2] = {};
+	Flipbook* _flipbookDead[2] = {};
+};
