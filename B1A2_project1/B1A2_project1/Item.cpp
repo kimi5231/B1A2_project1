d493ca230a1ed4afcee594899ba029ee08a97f8c
Im caheeun <dev.chaeng@gmail.com>
2025-03-13 14:23:16 +0900

    수정된 아이템 csv 파일 읽어오는 부분 수정 완료

--- B1A2_project1/B1A2_project1/Item.cpp
+++ B1A2_project1/B1A2_project1/Item.cpp
@@ -29,21 +29,50 @@ void Item::LoadFile(const std::wstring& path)
         std::wstringstream wiss(line);
         auto* info = new ItemInfo(); // 동적 할당
 
+        // ID
         std::wstring strID;
         std::getline(wiss, strID, L',');
         info->ID = std::stoi(strID);
 
+        // 아이템 이름
         std::getline(wiss, info->name, L',');
         
-        std::getline(wiss, info->explain);
-        // 개행 문자
-        int32 start = 0;
-        while ((start = info->explain.find(L"\\n", start)) != std::wstring::npos)
+        // 설명 
+        std::wstring strExplain;
+        std::getline(wiss, strExplain, L',');
+
+        // 개행 문자 "\n" 처리
+        size_t start = 0;
+        while ((start = strExplain.find(L"\\n", start)) != std::wstring::npos)
         {
-            info->explain.replace(start, 2, L"\n");
-            start += 1;
+            strExplain.replace(start, 2, L"\n");
+            start += 1; // 다음 탐색 위치 (중복 변환 방지)
         }
 
+        // F 키로 습득
+        std::wstring strAcquiredByFkey;
+        std::getline(wiss, strAcquiredByFkey, L',');
+        int32 intAcquiredByFkey = std::stoi(strAcquiredByFkey);
+        if (intAcquiredByFkey == -1)
+            info->acquiredByFKey = false;
+        else
+            info->acquiredByFKey = true;
+
+        // 드랍하는 몬스터 고유 ID - 없으면 0
+        std::wstring strDroppingMonsterPersonalID;
+        std::getline(wiss, strDroppingMonsterPersonalID, L',');
+        info->droppingMonsterPersonalID = std::stoi(strDroppingMonsterPersonalID);
+
+        // 스폰 위치 - 없으면 음수값
+        std::wstring strSpawnPosX;
+        std::getline(wiss, strSpawnPosX, L',');
+        info->spawnPos.x = std::stoi(strSpawnPosX);
+
+        std::wstring strSpawnPosY;
+        std::getline(wiss, strSpawnPosY, L',');
+        info->spawnPos.y = std::stoi(strSpawnPosY);
+
+        
         // 메모리 누수 방지용 - 근데 지금은 필요없어서 보류...
         //if (!std::getline(wiss, info->ID, L',')) { delete info; continue; }
         //if (!std::getline(wiss, info->name, L',')) { delete info; continue; }
