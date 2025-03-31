23f676380ca070dec0b8631240bb2fabd0ea2691
Lim Sooyoung <djwjod@naver.com>
2025-03-31 09:19:30 +0900

    EndingScene 클래스 추가

---
+++ B1A2_project1/B1A2_project1/EndingScene.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "EndingScene.h"
+
+EndingScene::EndingScene()
+{
+}
+
+EndingScene::~EndingScene()
+{
+}
+
+void EndingScene::Init()
+{
+}
+
+void EndingScene::Update()
+{
+}
+
+void EndingScene::Render(HDC hdc)
+{
+}
